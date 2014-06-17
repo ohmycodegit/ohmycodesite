6a825a2712401cb8c2c2c248cabca448cddb0f1b
LeDiegue <diegoflores1991@gmail.com>
2014-06-16 23:00:43 -0300

    added project folders

---
+++ presentation.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>OhMYCode Presentation members</title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/presentation.js"></script>
+</head>
+<body>
+
+
+</body>
+</html>
